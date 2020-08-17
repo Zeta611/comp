7975ec43a4c7128634dcbd445ba1b049492cede5
Jaeho Lee <jaeho.lee@snu.ac.kr>
2020-08-17 14:47:22 +0000

    :sparkles: Return outputs from `run-fsm`.

--- fsm.rkt
+++ fsm.rkt
@@ -1,8 +1,8 @@
 #lang racket
 (provide make-fsm get-state get-transition step run-fsm)
 
-; transition is a procedure that returns the next state when state and input
-; is given.
+; `transition` is a procedure that returns a pair of the next state and output
+; when the current state and an input is given.
 (define (make-fsm initial-state transition)
   (cons initial-state transition))
 
@@ -13,47 +13,55 @@
   (make-fsm next-state
             (get-transition fsm)))
 
+; Returns a pair of a next state and an output.
 (define (step fsm input)
   (let ((state (get-state fsm))
         (transition (get-transition fsm)))
     (transition state input)))
 
+; Returns a list of outputs processed from `inputs`.
 (define (run-fsm fsm inputs)
-  (unless (null? inputs)
-    (let ((next-state (step fsm (car inputs))))
-      (run-fsm 
-        (set-state fsm next-state)
-        (cdr inputs)))))
+  (if (null? inputs)
+    '()
+    (let* ((pair (step fsm (car inputs)))
+           (next-state (car pair))
+           (output (cdr pair)))
+      (cons output
+            (run-fsm
+              (set-state fsm next-state)
+              (cdr inputs))))))
 
 ; TEST CODE
 (define (code-trans state input)
-  (display state)
-  (newline)
   (match (cons state input)
     [(cons 'doc (or '@begin-doc '@show '@hide))
      (error "Invalid transition -- CODE-TRANS" state input)]
     [(cons 'doc '@end-doc)
-     'shown]
-    [(cons 'doc 'text)
-     'doc]
+     (cons 'shown '@end-doc)]
+    [(cons 'doc text)
+     (cons 'doc text)]
     [(cons 'shown (or '@end-doc '@show))
      (error "Invalid transition -- CODE-TRANS" state input)]
     [(cons 'shown '@begin-doc)
-     'doc]
+     (cons 'doc '@begin-doc)]
     [(cons 'shown '@hide)
-     'hidden]
-    [(cons 'shown 'text)
-     'shown]
+     (cons 'hidden '@hide)]
+    [(cons 'shown text)
+     (cons 'shown text)]
     [(cons 'hidden (or '@end-doc '@hide))
      (error "Invalid transition -- CODE-TRANS" state input)]
     [(cons 'hidden '@begin-doc)
-     'doc]
+     (cons 'doc '@begin-doc)]
     [(cons 'hidden '@show)
-     'shown]
-    [(cons 'hidden 'text)
-     'hidden]))
+     (cons 'shown '@show)]
+    [(cons 'hidden text)
+     (cons 'hidden text)]))
 
 (define sample-fsm
   (make-fsm 'shown code-trans))
 
-(run-fsm sample-fsm (list '@hide '@show))
+(display
+  (run-fsm
+    sample-fsm
+    (list '@hide '@show "(define x 1)" '@begin-doc "Hello, Doc!" '@end-doc)))
+(newline)
